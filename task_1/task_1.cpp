f7487d4660695c3adac2c3f4b7eb0d491f0e3d78
hellgirl1304 <hellgirl1304@gmail.com>
2024-06-28 17:54:21 +0500

    Igor`

---
+++ task_1/task_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+    int c1, b1;
+    std::cin >> c1 >> b1;
+    int hours = 2 * c1;
+    int firelessCnt = c1;
+    while (firelessCnt >= b1) {
+        hours += (firelessCnt / b1) * 4;
+        firelessCnt = firelessCnt % b1 + 2 * (firelessCnt / b1);
+    }
+    std::cout << hours << "\n";
+}
